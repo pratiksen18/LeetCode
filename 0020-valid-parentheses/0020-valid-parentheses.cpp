4a7783f632a1f494770bb018f87c53b7e7f643d1
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2023-06-20 11:43:31 +0530

    Time: 4 ms (40.32%), Space: 6.4 MB (40.16%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,39 @@
+// C++ Solution:
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Valid Parentheses.
+class Solution {
+public:
+    bool isValid(string s) {
+        // Initialize a stack and a index idx = 0...
+        stack<char> stack;
+        int idx = 0;
+        // If the string is empty, return true...
+        if(s.size() == 0){
+            return true;
+        }
+        // Create a loop to check parentheses...
+        while(idx < s.size()){
+            // If it contains the below parentheses, push the char to stack...
+            if( s[idx] == '(' || s[idx] == '[' || s[idx] == '{' ){
+                stack.push(s[idx]);
+            }
+            // If the current char is a closing brace provided, pop the top element...
+            // Stack is not empty...
+            else if ( (s[idx] == ')' && !stack.empty() && stack.top() == '(') ||
+                        (s[idx] == '}' && !stack.empty() && stack.top() == '{') ||
+                        (s[idx] == ']' && !stack.empty() && stack.top() == '[')
+                      ){
+                stack.pop();
+            }
+            else {
+                return false;       // If The string is not a valid parenthesis...
+            }
+            idx++;      // Increase the index...
+        }
+        // If stack.empty(), return true...
+        if(stack.empty()) {
+            return true;
+        }
+        return false;
+    }
+};
+
