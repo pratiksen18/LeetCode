43ca60a9984f7da12be920c0a4960370e8a4f6d2
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2023-06-20 12:03:47 +0530

    Time: 7 ms (86.01%), Space: 41.2 MB (99.46%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,26 @@
+// Java Solution:
+// Runtime: 3 ms, faster than 94.17% of Java online submissions for Isomorphic Strings.
+// Memory Usage: 42.1 MB, less than 95.61% of Java online submissions for Isomorphic Strings.
+// Time Complexity : O(n)
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        // Base case: for different length of two strings...
+        if(s.length() != t.length())
+            return false;
+        // Create two maps for s & t strings...
+        int[] map1 = new int[256];
+        int[] map2 = new int[256];
+        // Traverse all elements through the loop...
+        for(int idx = 0; idx < s.length(); idx++){
+            // Compare the maps, if not equal, return false...
+            if(map1[s.charAt(idx)] != map2[t.charAt(idx)])
+                return false;
+            // Insert each character if string s and t into seperate map...
+            map1[s.charAt(idx)] = idx + 1;
+            map2[t.charAt(idx)] = idx + 1;
+        }
+        return true;    // Otherwise return true...
+    }
+}
+
+
