77e66c31a0f3eff7649f86195752ec700a433ff1
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2023-06-20 11:47:17 +0530

    C++ || Java || Python || JS || Python3 (320 Upvotes)
    320 upvotes in this solution

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -37,3 +37,103 @@ public:
     }
 };
 
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Java Solution (Using Hashmap):
+class Solution {
+    public boolean isValid(String s) {
+        // Create hashmap to store the pairs...
+        HashMap<Character, Character> Hmap = new HashMap<Character, Character>();
+        Hmap.put(')','(');
+        Hmap.put('}','{');
+        Hmap.put(']','[');
+        // Create stack data structure...
+        Stack<Character> stack = new Stack<Character>();
+        // Traverse each charater in input string...
+        for (int idx = 0; idx < s.length(); idx++){
+            // If open parentheses are present, push it to stack...
+            if (s.charAt(idx) == '(' || s.charAt(idx) == '{' || s.charAt(idx) == '[') {
+                stack.push(s.charAt(idx));
+                continue;
+            }
+            // If the character is closing parentheses, check that the same type opening parentheses is being pushed to the stack or not...
+            // If not, we need to return false...
+            if (stack.size() == 0 || Hmap.get(s.charAt(idx)) != stack.pop()) {
+                return false;
+            }
+        }
+        // If the stack is empty, return true...
+        if (stack.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+}
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python Solution:
+class Solution(object):
+    def isValid(self, s):
+        # Create a pair of opening and closing parrenthesis...
+        opcl = dict(('()', '[]', '{}'))
+        # Create stack data structure...
+        stack = []
+        # Traverse each charater in input string...
+        for idx in s:
+            # If open parentheses are present, append it to stack...
+            if idx in '([{':
+                stack.append(idx)
+            # If the character is closing parentheses, check that the same type opening parentheses is being pushed to the stack or not...
+            # If not, we need to return false...
+            elif len(stack) == 0 or idx != opcl[stack.pop()]:
+                return False
+        # At last, we check if the stack is empty or not...
+        # If the stack is empty it means every opened parenthesis is being closed and we can return true, otherwise we return false...
+        return len(stack) == 0
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                    
+// JavaScript Solution:
+var isValid = function(s) {
+    // Initialize stack to store the closing brackets expected...
+    let stack = [];
+    // Traverse each charater in input string...
+    for (let idx = 0; idx < s.length; idx++) {
+        // If open parentheses are present, push it to stack...
+        if (s[idx] == '{') {
+            stack.push('}');
+        } else if (s[idx] == '[') {
+            stack.push(']');
+        } else if (s[idx] == '(') {
+            stack.push(')');
+        }
+        // If a close bracket is found, check that it matches the last stored open bracket
+        else if (stack.pop() !== s[idx]) {
+            return false;
+        }
+    }
+    return !stack.length;
+};
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python3 Solution:
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Create a pair of opening and closing parrenthesis...
+        opcl = dict(('()', '[]', '{}'))
+        # Create stack data structure...
+        stack = []
+        # Traverse each charater in input string...
+        for idx in s:
+            # If open parentheses are present, append it to stack...
+            if idx in '([{':
+                stack.append(idx)
+            # If the character is closing parentheses, check that the same type opening parentheses is being pushed to the stack or not...
+            # If not, we need to return false...
+            elif len(stack) == 0 or idx != opcl[stack.pop()]:
+                return False
+        # At last, we check if the stack is empty or not...
+        # If the stack is empty it means every opened parenthesis is being closed and we can return true, otherwise we return false...
+        return len(stack) == 0
