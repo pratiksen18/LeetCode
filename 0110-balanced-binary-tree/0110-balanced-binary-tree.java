ac126ff93a404db746059933b16ed41889fb17c0
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2023-06-21 09:09:42 +0530

    Java || C++ || Python || JS || Python3 (220 Upvotes)
    220 upvotes in this solution

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -41,3 +41,79 @@ class Solution {
     }
 }
 
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// C++ Solution:
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        // If the tree is empty, we can say it’s balanced...
+        if (root == NULL)  return true;
+        // Height Function will return -1, when it’s an unbalanced tree...
+		if (Height(root) == -1)  return false;
+		return true;
+	}
+    // Create a function to return the “height” of a current subtree using recursion...
+	int Height(TreeNode* root) {
+        // Base case...
+		if (root == NULL)  return 0;
+        // Height of left subtree...
+		int leftHeight = Height(root->left);
+        // Height of height subtree...
+		int rightHight = Height(root->right);
+        // In case of left subtree or right subtree unbalanced or their heights differ by more than ‘1’, return -1...
+		if (leftHeight == -1 || rightHight == -1 || abs(leftHeight - rightHight) > 1)  return -1;
+        // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
+		return max(leftHeight, rightHight) + 1;
+    }
+};
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python Solution:
+class Solution(object):
+    def isBalanced(self, root):
+        return (self.Height(root) >= 0)
+    def Height(self, root):
+        if root is None:  return 0
+        leftheight, rightheight = self.Height(root.left), self.Height(root.right)
+        if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:  return -1
+        return max(leftheight, rightheight) + 1
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            
+// JavaScript Solution:
+var isBalanced = function(root) {
+    // If the tree is empty, we can say it’s balanced...
+    if (root == null)  return true;
+    // Height Function will return -1, when it’s an unbalanced tree...
+	if (Height(root) == -1)  return false;
+	return true;
+}
+// Create a function to return the “height” of a current subtree using recursion...
+var Height = function(root) {
+    // Base case...
+	if (root == null)  return 0;
+    // Height of left subtree...
+	let leftHeight = Height(root.left);
+    // Height of height subtree...
+	let rightHight = Height(root.right);
+    // In case of left subtree or right subtree unbalanced, return -1...
+	if (leftHeight == -1 || rightHight == -1)  return -1;
+    // If their heights differ by more than ‘1’, return -1...
+    if (Math.abs(leftHeight - rightHight) > 1)  return -1;
+    // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
+	return Math.max(leftHeight, rightHight) + 1;
+};
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python3 Solution:
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return (self.Height(root) >= 0)
+    def Height(self, root: Optional[TreeNode]) -> bool:
+        if root is None:  return 0
+        leftheight, rightheight = self.Height(root.left), self.Height(root.right)
+        if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:  return -1
+        return max(leftheight, rightheight) + 1
