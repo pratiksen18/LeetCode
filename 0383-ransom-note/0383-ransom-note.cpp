f4e91c0f32fc93214f32f49d6feb1533a0d9bdc0
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2023-06-21 09:22:32 +0530

    C++ || Java || Python || Python3 (160 Upvotes)
    160 upvotes in this solution

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -16,3 +16,31 @@ public:
     }
 };
 
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Java Solution:
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        // Initialize an array of count with the size 26...
+        int[] count = new int[128];
+        // Traverse a loop through the entire String of magazine where char ch stores the char at the index of magazine...
+        for (final char ch : magazine.toCharArray())
+            ++count[ch];
+        // Run another for loop for ransomNote...
+        for (final char ch : ransomNote.toCharArray())
+             // If the charachter doesn't exists in magazine for ransomNote, we return false...
+            if (--count[ch] < 0)
+                return false;
+        return true;      // If nothing goes wrong, return true...
+    }
+}
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python/Python3 Solution:
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        st1, st2 = Counter(ransomNote), Counter(magazine)
+        if st1 & st2 == st1:
+            return True
+        return False
