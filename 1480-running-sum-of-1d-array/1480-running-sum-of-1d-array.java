50658cc7f6f603bbeefc0790f9a97990b0d86df4
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2023-06-22 10:58:17 +0530

    Java || C++ || Python || JS || Python3 (160 Upvotes)
    160 upvotes in this solution

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,4 +1,5 @@
 // Java Solution:
+
 // Runtime: 0 ms, faster than 100.00% of Java online submissions for Running Sum of 1d Array.
 // Time Complexity : O(n)
 // Space Complexity : O(n)
@@ -20,3 +21,62 @@ class Solution {
     }
 }
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// C++ Solution:
+    
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        // Create an output array of size equal to given nums size...
+        vector<int> output(nums.size());
+        // Base case: if the array is empty...
+        if(nums.size() == 0)
+            return output;
+        // Set output[0] = nums[0]...
+        output[0] = nums[0];
+        // Traverse all elements through the for loop...
+        for(int idx = 1; idx < nums.size(); idx++) {
+            // Storing the running sum...
+            output[idx] = output[idx-1] + nums[idx];
+        } return output;     // Return the running sum of nums...
+    }
+};
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python/Python3 Solution:
+    
+# Time Complexity : O(n)
+# Space Complexity : O(n)
+class Solution(object):
+    def runningSum(self, nums):
+        # Create an output array of size equal to given nums size...
+        output = [0] * len(nums)
+        # Set output[0] = nums[0]...
+        output[0] = nums[0]
+        # Traverse all elements through the for loop...
+        for idx in range(1, len(nums)):
+            # Storing the running sum...
+            output[idx] = output[idx - 1] + nums[idx]
+        return output       # Return the running sum of nums...
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                
+// JavaScript Solution:
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+var runningSum = function(nums) {
+    // Create an output array of size equal to given nums size...
+    let output = new Array(nums.length)
+    // Set output[0] = nums[0]...
+    output[0] = nums[0];
+    // Traverse all elements through the for loop...
+    for(let idx = 1; idx < nums.length; idx++) {
+        // Storing the running sum...
+        output[idx] = output[idx-1] + nums[idx];
+    }
+    return output;      // Return the running sum of nums...
+};
