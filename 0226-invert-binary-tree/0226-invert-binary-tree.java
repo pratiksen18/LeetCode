92fc69c3d7604453891f26838c2c576bd3b41c96
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2023-06-22 11:11:22 +0530

    Java || C++ || Python || JS || C Language || Python3 (140 Upvotes)
    140 upvotes in this solution

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -36,3 +36,111 @@ class Solution {
     }
 }
 
+/**Iterative Approach:**/
+// Runtime: 1 ms, faster than 90.96% of Java online submissions for Invert Binary Tree.
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        LinkedList<TreeNode> q = new LinkedList<TreeNode>();
+        // Base case: if the tree is empty...
+        if(root != null){
+            // Push the root node...
+            q.add(root);
+        }
+        // Loop till queue is empty...
+        while(!q.isEmpty()){
+            // Dequeue front node...
+            TreeNode temp = q.poll();
+            // Enqueue left child of the popped node...
+            if(temp.left != null)
+                q.add(temp.left);
+            // Enqueue right child of the popped node
+            if(temp.right != null)
+                q.add(temp.right);
+            // Swapping process...
+            TreeNode curr = temp.left;
+            temp.left = temp.right;
+            temp.right = curr;
+        }
+         return root;    // Return the root...
+    }
+}
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// C++ Solution (Recursive Approach):
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        // Base case...
+        if(root == NULL){
+            return root;
+        }
+        // Call the function recursively for the left subtree...
+        invertTree(root->left);
+        // Call the function recursively for the right subtree...
+        invertTree(root->right);
+        // swapping process...
+        TreeNode* curr = root->left;
+        root->left = root->right;
+        root->right = curr;
+        return root;        // Return the root...
+    }
+};
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python / Python3 Solution:
+class Solution(object):
+    def invertTree(self, root):
+        # Base case...
+        if root == None:
+            return root
+        # swapping process...
+        root.left, root.right = root.right, root.left
+        # Call the function recursively for the left subtree...
+        self.invertTree(root.left)
+        # Call the function recursively for the right subtree...
+        self.invertTree(root.right)
+        return root     # Return the root...
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            
+// JavaScript Solution (Recursive Approach):
+var invertTree = function(root) {
+    // Base case...
+    if(root == null){
+        return root;
+    }
+    // Call the function recursively for the left subtree...
+    invertTree(root.left);
+    // Call the function recursively for the right subtree...
+    invertTree(root.right);
+    // swapping process...
+    const curr = root.left;
+    root.left = root.right;
+    root.right = curr;
+    return root;        // Return the root...   
+};
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// C Language (Recursive Approach):
+struct TreeNode* invertTree(struct TreeNode* root){
+    // Base case...
+    if(root == NULL){
+        return root;
+    }
+    // Call the function recursively for the left subtree...
+    invertTree(root->left);
+    // Call the function recursively for the right subtree...
+    invertTree(root->right);
+    // swapping process...
+    struct TreeNode* curr = root->left;
+    root->left = root->right;
+    root->right = curr;
+    return root;        // Return the root...
+}
