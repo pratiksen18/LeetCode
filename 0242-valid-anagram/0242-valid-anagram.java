1e3bf9925100da03de7260c3644f9cd0ab8daaf2
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2023-06-22 11:15:11 +0530

    Java || C++ || Python || JS || Python3 (130 Upvotes)
    130 upvotes in this solution

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -25,3 +25,67 @@ class Solution {
     }
 }
 
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// C++ Solution:
+// If two strings are anagrams then the frequency of every char in both of the strings are same.
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // In case of different length of thpse two strings...
+        if(s.length() != t.length()) return false;
+        // Count the freq of every character in s & t...
+        vector<int> counter1(26,0), counter2(26,0);
+        // Traverse through the loop...
+        for(int idx = 0; idx < s.length(); idx++) {
+            counter1[s[idx]-'a']++;
+            counter2[t[idx]-'a']++;
+        }
+        // Compare freq1[i] and freq2[i] for every index i from 0 to 26...
+        for(int idx = 0; idx < 26; idx++) {
+            // If they are different, return false...
+            if(counter1[idx] != counter2[idx]) return false;
+        }
+        return true;        // Otherwise, return true...
+
+    }
+};
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python/Python3 Solution:
+class Solution(object):
+    def isAnagram(self, s, t):
+        # In case of different length of thpse two strings...
+        if len(s) != len(t):
+            return False
+        for idx in set(s):
+            # Compare s.count(l) and t.count(l) for every index i from 0 to 26...
+            # If they are different, return false...
+            if s.count(idx) != t.count(idx):
+                return False
+        return True     # Otherwise, return true...
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// JavaScript Solution:
+var isAnagram = function(s, t) {
+    // To count freq we make an array of size 26...
+        const counter = new Array(26).fill(0);
+        // Traverse all elements through a loop...
+        for(let idx = 0; idx < s.length; idx++){
+            counter[s.charCodeAt(idx)-97]++;
+        }
+        for(let idx = 0; idx < t.length; idx++){
+            counter[t.charCodeAt(idx)-97]--;
+        }
+        // Above iteration provides us with count array having all values to zero then we can say we found an anagram.
+        // Every element of count has to be equal to 0.
+        // If it is greater than 0 it means s has a character whose occurrence is greater than its occurrence in t.
+        // And if its less than 0 then, s has a character whose occurrence is smaller than its occurrence in t.
+        for (let idx = 0; idx < 26; idx++) {
+            if(counter[idx] != 0)
+                return false;
+        }
+        return true;
+};
