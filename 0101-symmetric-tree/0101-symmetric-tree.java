9ae9fd7e2fa58b106f5a817f144fbda35186f125
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2023-06-23 13:56:08 +0530

    Java || C++ || Python || JS || Python3 (100 Upvotes)
    100 upvotes in this solution

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -13,8 +13,10 @@
  *     }
  * }
  */
+
 // Java Solution:
 // Runtime: 0 ms, faster than 100.00% of Java online submissions for Symmetric Tree.
+
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         // Soecial case...
@@ -47,3 +49,102 @@ class Solution {
     }
 }
 
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// C++ Solution:
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        // Special case...
+        if(root == nullptr) return true;
+        // Return the function recursively...
+        return isSymmetric(root->left,root->right);
+    }
+    // A tree is called symmetric if the left subtree must be a mirror reflection of the right subtree...
+    bool isSymmetric(TreeNode* leftroot,TreeNode* rightroot){
+        // If both root nodes are null pointers, return true...
+        if(!leftroot && !rightroot)  return true;
+        // If exactly one of them is a null node, return false...
+        if(!leftroot || !rightroot)  return false;
+        // If root nodes haven't same value, return false...
+        if(leftroot->val != rightroot->val)  return false;
+        // Return true if the values of root nodes are same and left as well as right subtrees are symmetric...
+        return isSymmetric(leftroot->left, rightroot->right) && isSymmetric(leftroot->right, rightroot->left);
+    }
+};
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python Solution:
+
+class Solution(object):
+    def isSymmetric(self, root):
+        # Special case...
+        if not root:
+            return true;
+        # Return the function recursively...
+        return self.isSame(root.left, root.right)
+    # A tree is called symmetric if the left subtree must be a mirror reflection of the right subtree...
+    def isSame(self, leftroot, rightroot):
+        # If both root nodes are null pointers, return true...
+        if leftroot == None and rightroot == None:
+            return True
+        # If exactly one of them is a null node, return false...
+        if leftroot == None or rightroot == None:
+            return False
+        # If root nodes haven't same value, return false...
+        if leftroot.val != rightroot.val:
+            return False
+        # Return true if the values of root nodes are same and left as well as right subtrees are symmetric...
+        return self.isSame(leftroot.left, rightroot.right) and self.isSame(leftroot.right, rightroot.left)
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// JavaScript Solution:
+// Runtime: 66 ms, faster than 97.80% of JavaScript online submissions for Symmetric Tree.
+
+var isSymmetric = function(root) {
+    // Special case...
+    if (!root)
+        return true;
+    // Return the function recursively...
+    return isSame(root.left, root.right);
+};
+// A tree is called symmetric if the left subtree must be a mirror reflection of the right subtree...
+var isSame = function (leftroot, rightroot) {
+    // If both root nodes are null pointers, return true...
+    // If exactly one of them is a null node, return false...
+    // If root nodes haven't same value, return false...
+    if ((!leftroot && rightroot) || (leftroot && !rightroot) || (leftroot && rightroot && leftroot.val !== rightroot.val))
+        return false;
+    // Return true if the values of root nodes are same and left as well as right subtrees are symmetric...
+    if (leftroot && rightroot)
+        return isSame(leftroot.left, rightroot.right) && isSame(leftroot.right, rightroot.left);
+    return true;
+};
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python3 Solution:
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        # Special case...
+        if not root:
+            return true;
+        # Return the function recursively...
+        return self.isSame(root.left, root.right)
+    # A tree is called symmetric if the left subtree must be a mirror reflection of the right subtree...
+    def isSame(self, leftroot, rightroot):
+        # If both root nodes are null pointers, return true...
+        if leftroot == None and rightroot == None:
+            return True
+        # If exactly one of them is a null node, return false...
+        if leftroot == None or rightroot == None:
+            return False
+        # If root nodes haven't same value, return false...
+        if leftroot.val != rightroot.val:
+            return False
+        # Return true if the values of root nodes are same and left as well as right subtrees are symmetric...
+        return self.isSame(leftroot.left, rightroot.right) and self.isSame(leftroot.right, rightroot.left)
