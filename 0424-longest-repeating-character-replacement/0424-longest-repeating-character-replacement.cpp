e2d2d34034fe3d5dfe050f22da2b6fe705cc27cb
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2023-06-23 13:39:47 +0530

    C++ || Java || Python || JS || Python3 (110 Upvotes)
    110 upvotes in this solution

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,8 +1,7 @@
-
-
 // C++ Solution:
 // Time Complexity :  O(n)
 // Space Complexity : O(1)
+
 class Solution {
 public:
     int characterReplacement(string s, int k) {
@@ -27,3 +26,84 @@ public:
     }
 };
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Java Solution:
+// Time Complexity :  O(n)
+// Space Complexity : O(1)
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        // Make an array of size 26...
+        int[] arr = new int[26];
+        // Initialize largestCount, maxlen & beg pointer...
+        int largestCount = 0, beg = 0, maxlen = 0;
+        // Traverse all characters through the loop...
+        for(int end = 0; end < s.length(); end ++){
+            arr[s.charAt(end) - 'A']++;
+            // Get the largest count of a single, unique character in the current window...
+            largestCount = Math.max(largestCount, arr[s.charAt(end) - 'A']);
+            // We are allowed to have at most k replacements in the window...
+            // So, if max character frequency + distance between beg and end is greater than k...
+            // this means we have considered changing more than k charactres. So time to shrink window...
+            // Then there are more characters in the window than we can replace, and we need to shrink the window...
+            if(end - beg + 1 - largestCount > k){     // The main equation is: end - beg + 1 - largestCount...
+                arr[s.charAt(beg) - 'A']--;
+                beg ++;
+            }
+            // Get the maximum length of repeating character...
+            maxlen = Math.max(maxlen, end - beg + 1);     // end - beg + 1 = size of the current window...
+        }
+        return maxlen;      // Return the maximum length of repeating character...
+    }
+}
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python/Python3 Solution:
+// Time Complexity :  O(n)
+// Space Complexity : O(1)
+    
+class Solution(object):
+    def characterReplacement(self, s, k):
+        maxlen, largestCount = 0, 0
+        arr = collections.Counter()
+        for idx in xrange(len(s)):
+            arr[s[idx]] += 1
+            largestCount = max(largestCount, arr[s[idx]])
+            if maxlen - largestCount >= k:
+                arr[s[idx - maxlen]] -= 1
+            else:
+                maxlen += 1
+        return maxlen
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// JavaScript Solution:
+// Time Complexity :  O(n)
+// Space Complexity : O(1)
+    
+var characterReplacement = function(s, k) {
+    // Make a map of size 26...
+    var map = [26]
+    // Initialize largestCount, maxlen & beg pointer...
+    let largestCount = 0, beg = 0, maxlen = 0;
+    // Traverse all characters through the loop...
+    for(let end = 0; end < s.length; end++){
+        const c = s[end]
+        map[c] = (map[c] || 0) + 1
+        // Get the largest count of a single, unique character in the current window...
+        largestCount = Math.max(largestCount, map[c])
+        // We are allowed to have at most k replacements in the window...
+        // So, if max character frequency + distance between beg and end is greater than k...
+        // this means we have considered changing more than k charactres. So time to shrink window...
+        // Then there are more characters in the window than we can replace, and we need to shrink the window...
+        if(end - beg + 1 - largestCount > k){     // The main equation is: end - beg + 1 - largestCount...
+            map[s[beg]] -= 1
+            beg += 1
+        }
+        // Get the maximum length of repeating character...
+        maxlen = Math.max(maxlen, end - beg + 1);     // end - beg + 1 = size of the current window...
+    }
+    return maxlen;      // Return the maximum length of repeating character...
+};
