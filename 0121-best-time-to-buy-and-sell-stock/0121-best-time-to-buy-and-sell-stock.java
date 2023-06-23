226b2db0c4b43c0ada78ebca0ea7c2bfaac09b89
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2023-06-23 13:50:56 +0530

    Java || C++ || Python || JS || Python3 (110 Upvotes)
    110 upvotes in this solution

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -22,3 +22,71 @@ class Solution {
     }
 }
 
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// C++ Solution:
+    
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() == 0) return 0;
+        int profit = 0;
+        int low = prices[0], high = prices[0];
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < low){
+                profit = max(profit, high - low);
+                low = prices[i];
+                high = prices[i];
+            }else{
+                high = max(high, prices[i]);
+            }
+        }
+        profit = max(profit, high - low);
+        return profit;
+    }
+};
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python Solution:
+    
+class Solution(object):
+    def maxProfit(self, prices):
+        if len(prices) == 0: return 0
+        else:
+            profit = 0
+            minBuy = prices[0]
+            for i in range(len(prices)):
+                profit = max(prices[i] - minBuy, profit)
+                minBuy = min(minBuy, prices[i])
+            return profit
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// JavaScript Solution:
+    
+var maxProfit = function(prices) {
+    if(prices == null || prices.length <= 1) return 0;
+    let minBuy = prices[0];
+    let profit = 0;
+    for(let i = 1; i < prices.length; i++) {
+        minBuy = Math.min(minBuy, prices[i]);
+        profit = Math.max(profit, prices[i] - minBuy);
+    }
+    return profit;
+};
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python3 Solution:
+    
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) == 0: return 0
+        else:
+            profit = 0
+            minBuy = prices[0]
+            for i in range(len(prices)):
+                profit = max(prices[i] - minBuy, profit)
+                minBuy = min(minBuy, prices[i])
+            return profit
