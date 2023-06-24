3a0150f3e9b2f08a20028411aad21560c3728fc7
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2023-06-24 09:04:07 +0530

    Java || C++ || Python || JS || Python3 (95 Upvotes)

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,5 +1,9 @@
+
+
+
 // Java Solution (Using HashSet):
 // Time Complexity : O(n)
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         // Base case...
@@ -24,3 +28,75 @@ class Solution {
         return false;
     }
 }
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// C++ Solution:
+// Time Complexity : O(n)
+// Space Complexity : O(min(k,n))
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        // Create a Set for storing previous of k elements...
+        unordered_set<int> hset;
+        // Traverse for all elements of the given array in a for loop...
+        for(int idx = 0; idx < nums.size(); idx++) {
+            // Check if set already contains nums[idx] or not...
+            // If duplicate element is present at distance less than equal to k, return true...
+            if(hset.count(nums[idx]))
+                return true;
+            // Otherwise, add nums[idx] to the set...
+            hset.insert(nums[idx]);
+            // If size of the set becomes greater than k...
+            if(hset.size() > k)
+                // Remove the last visited element from the set...
+                hset.erase(nums[idx - k]);
+        }
+        // If no duplicate element is found then return false...
+        return false;
+    }
+};
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python Solution (1 Line):
+
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        return len(list(set(nums))) < len(nums)
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// JavaScript Solution:
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+    
+var containsNearbyDuplicate = function(nums, k) {
+    const hasmap = new Map();
+    for (let idx = 0; idx < nums.length; idx++) {
+        // Check if the difference betweend duplicates is less than k
+        if (idx - hasmap.get(nums[idx]) <= k) {
+            return true;
+        }
+        hasmap.set(nums[idx], idx);
+    }
+    return false;
+};
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python3 Solution:
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # Create hset for storing previous of k elements...
+        hset = {}
+        # Traverse for all elements of the given array in a for loop...
+        for idx in range(len(nums)):
+            # If duplicate element is present at distance less than equal to k, return true...
+            if nums[idx] in hset and abs(idx - hset[nums[idx]]) <= k:
+                return True
+            hset[nums[idx]] = idx
+        # If no duplicate element is found then return false...
+        return False
