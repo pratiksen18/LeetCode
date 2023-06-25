cb28b8ab1c664ce19d872ee32e1f1827a0fef5e1
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2023-06-25 12:18:05 +0530

    Java || C++ || Python || Python3 (70 Upvotes)
    70 upvotes in this solution

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -25,3 +25,47 @@ class Solution {
         return output.toString();
     }
 }
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// C++ Solution:
+
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        // Create an empty string for storing the characters...
+        string output;
+        // Run a while loop while columnNumber is positive...
+        while(columnNumber > 0){
+            // Subtract 1 from columnNumber...
+            columnNumber--;
+            // Get current character by doing modulo of columnNumber by 26...
+            int m = columnNumber % 26;
+            // Divide columnNumber by 26...
+            columnNumber /= 26;
+            output += 'A' + m;
+        }
+        // Reverse the output string...
+        // Bcause we have found characters from right to left...
+        reverse(output.begin(),output.end());
+        // Return the reversed string.
+        return output;
+    }
+};
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python / Python3 Solution:
+
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        # Create an empty string for storing the characters...
+        output = ""
+        # Run a while loop while columnNumber is positive...
+        while columnNumber > 0:
+            # Subtract 1 from columnNumber and get current character by doing modulo of columnNumber by 26...
+            output = chr(ord('A') + (columnNumber - 1) % 26) + output
+            # Divide columnNumber by 26...
+            columnNumber = (columnNumber - 1) // 26
+        # Return the output string.
+        return output
