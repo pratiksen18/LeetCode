f53388f6609007e60dccc790b75049e6fcf7f85e
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2023-06-28 14:48:19 +0530

    Java || C++ || Python || JS || C Language || Python3 (30 Upvotes)
    30 upvotes in this solution

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -29,3 +29,101 @@ public class Solution {
         return false;
     }
 }
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// C++ Solution:
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // Initialize two node slow and fast point to the hesd node...
+        ListNode* fastptr = head;
+        ListNode* slowptr = head;
+        while(fastptr != NULL && fastptr->next != NULL){
+            // Move slow pointer by 1 node and fast at 2 at each step.
+            slowptr = slowptr->next;
+            fastptr = fastptr->next->next;
+            // If both the pointers meet at any point, then the cycle is present and return true...
+            if(slowptr == fastptr)
+                return true;
+        }
+        return false;
+    }
+};
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python Solution:
+
+class Solution(object):
+    def hasCycle(self, head):
+        # Initialize two node slow and fast point to the hesd node...
+        fastptr = head
+        slowptr = head
+        while fastptr is not None and fastptr.next is not None:
+            # Move slow pointer by 1 node and fast at 2 at each step.
+            slowptr = slowptr.next
+            fastptr = fastptr.next.next
+            # If both the pointers meet at any point, then the cycle is present and return true...
+            if slowptr == fastptr:
+                return 1
+        return 0
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------        
+                    
+// JavaScript Solution:
+                    
+var hasCycle = function(head) {
+    // Initialize two node slow and fast point to the hesd node...
+    var fastptr = head;
+    var slowptr = head;
+    while(fastptr != null && fastptr.next != null){
+        // Move slow pointer by 1 node and fast at 2 at each step.
+        slowptr = slowptr.next;
+        fastptr = fastptr.next.next;
+        // If both the pointers meet at any point, then the cycle is present and return true...
+        if(slowptr == fastptr)
+            return true;
+    }
+    return false;
+};
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// C Language:
+
+bool hasCycle(struct ListNode *head) {
+    // Initialize two node slow and fast point to the hesd node...
+    struct ListNode* fastptr = head;
+    struct ListNode* slowptr = head;
+    while(fastptr != NULL && fastptr->next != NULL){
+        // Move slow pointer by 1 node and fast at 2 at each step.
+        slowptr = slowptr->next;
+        fastptr = fastptr->next->next;
+        // If both the pointers meet at any point, then the cycle is present and return true...
+        if(slowptr == fastptr)
+            return true;
+    }
+    return false;
+}
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Python3 Solution:
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # Initialize two node slow and fast point to the hesd node...
+        fastptr = head
+        slowptr = head
+        while fastptr is not None and fastptr.next is not None:
+            # Move slow pointer by 1 node and fast at 2 at each step.
+            slowptr = slowptr.next
+            fastptr = fastptr.next.next
+            # If both the pointers meet at any point, then the cycle is present and return true...
+            if slowptr == fastptr:
+                return 1
+        return 0
+                    
+                    
