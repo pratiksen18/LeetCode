898a92f5573026db583331bdcd476c42f1779f27
Pratik Sen <132545571+pratiksen18@users.noreply.github.com>
2023-07-15 20:29:20 +0530

    Time: 44 ms (98.00%), Space: 41.4 MB (87.38%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,13 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function() {
+        return "Hello World";
+  }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
